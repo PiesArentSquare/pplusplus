9bd0d8ed5bed581300506644c8661fbbf2762c3e
PiesArentSquare <jjhh12@outlook.com>
2020-04-27 19:42:59 -0400

    modularized adding of buildfile keys

--- src/buildfile_parser.cpp
+++ src/buildfile_parser.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <stdio.h>
 #include <fstream>
 #include <vector>
 #include <map>
@@ -25,7 +25,7 @@ strvec tokenize_file(const char* filepath, const char* delimiters = " ") {
 
 	// Error check
 	if (file.fail()) {
-		std::cerr << "tokenizer error: file '" << filepath << "' not found.\n";
+		fprintf(stderr, "tokenizer error: file '%s' not found.\n", filepath);
 		return {};
 	}
 
@@ -39,26 +39,39 @@ strvec tokenize_file(const char* filepath, const char* delimiters = " ") {
 
 typedef std::map<std::string, strvec> svmap;
 
-svmap parse_file(const char* filepath, std::string mode) {
+void insert(svmap &map, strvec &vec, std::string key) {
+	auto it = map.find(key);
+	if (it != map.end()) {
+		auto v = map[key];
+		v.insert(v.end(), vec.begin(), vec.end());
+		it->second = v;
+	} else map.insert({key, vec});
+}
+
+
+svmap parse_file(const char* filepath, const std::string profile) {
 
 	strvec tokens = tokenize_file(filepath);
 
 	const std::string os =
-	#if defined(unix) || defined(__unix) || defined(__unix__)
-	"unix";
-	#elif defined(_WIN32)
+	#if defined(_WIN32)
 	"windows";
 	#elif defined(__linux__)
 	"linux";
 	#elif defined(__APPLE__)
 	"osx";
 	#endif
-	
+
+	const std::string isUnix = "";
+	#if defined(unix) || defined(__unix) || defined(__unix__)
+	isUnix = "unix";
+	#endif
+
 	svmap objMap;
 
 	bool isKey = true, isStr = false, isArr = false;
 	// 0 = invaliid, 1 = valid, 2 = checking
-	uint8_t osValid = 1, modeValid = 1;
+	uint8_t osValid = 1, profileValid = 1;
 
 	std::string objKey, tempVal;
 	strvec objVec;
@@ -67,22 +80,22 @@ svmap parse_file(const char* filepath, std::string mode) {
 		// Check if string
 		if(token == "\"") isStr = !isStr;
 		else {
-			// Check os and mode compatibility
+			// Check os and profile compatibility
 			if (token == "#") {
-				if (osValid >= 2) { modeValid = 2; osValid -= 2; }
+				if (osValid >= 2) { profileValid = 2; osValid -= 2; }
 				else osValid += 2;
 			}
-			else if (modeValid == 2) {
-				if (token == mode || token == "*") modeValid = 1;
-				else modeValid = 0;
+			else if (profileValid == 2) {
+				if (token == profile || token == "*") profileValid = 1;
+				else profileValid = 0;
 			}
 			else if (osValid >= 2) {
-				if (token == os || token == "*") osValid = 1;
+				if (token == os || token == isUnix || token == "*") osValid = 1;
 				else osValid = 0;
 			}
 
-			// Valid os and mdoe
-			else if (osValid == 1 && modeValid == 1) {
+			// Valid os and profile
+			else if (osValid == 1 && profileValid == 1) {
 				if (isKey) {
 					if (token == ":" && !isStr) isKey = false;
 					else objKey += token;
@@ -91,10 +104,12 @@ svmap parse_file(const char* filepath, std::string mode) {
 					else if (token == "[") isArr = true;
 					else if (token == "]") isArr = false;
 					else if (token == ",") {
-						objVec.push_back(tempVal);
-						tempVal = "";
+						if (!tempVal.empty()) {
+							objVec.push_back(tempVal);
+							tempVal = "";
+						}
 						if (!isArr) {
-							objMap.insert({objKey, objVec});
+							insert(objMap, objVec, objKey);
 							objKey = "";
 							objVec = {};
 							isKey = true;
@@ -104,11 +119,10 @@ svmap parse_file(const char* filepath, std::string mode) {
 			}
 
 		}
-
 	}
-	if (!tempVal.empty()) {
+	if (!tempVal.empty() || !isArr) {
 		objVec.push_back(tempVal);
-		objMap.insert({objKey, objVec});
+		insert(objMap, objVec, objKey);
 	}
 
 	return objMap;
--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,13 @@ namespace fs = std::filesystem;
 
 #include "buildfile_parser.cpp"
 
-void usage();
-
 void searchDir(fs::path root, std::vector<fs::path> excludePaths, std::string& fileString);
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
 
 	// Send a usage statement if no arguments are provided
 	if (argc < 2) {
-		usage();
+		printf("usage: p++ requires one (or more) source directory(s)\n");
 		return 0;
 	}
 
@@ -22,32 +20,41 @@ int main(int argc, char** argv) {
 	bool hasOutDir = false;
 
 	// Lists of paths to include or exclude from compilation
-	std::vector<fs::path> includePaths, excludePaths;
+	std::vector<fs::path> rootPaths, excludePaths;
 
 	// If there is a buildfile read it and add g++ flags accordingly
-	std::string arg1 = argv[1];
+	std::string arg1 = std::string(argv[1]);
 	if (arg1.substr(0, 2) == "_b") {
-		const char* mode = "";
-		if (argc > 2) mode = argv[2];
-		svmap objects = parse_file((arg1.size() > 2) ? (arg1.substr(2) + ".ppp").c_str() : "build.ppp", mode);
-	
-		fs::path baseDir = fs::path(arg1.substr(2)).parent_path();
 
-		for (svmap::iterator it = objects.begin(); it != objects.end(); it++) {
-			for (auto value : it->second) {
-				fs::path relPath = baseDir / fs::path(value);
-				if (it->first == "lib") compileFlags += "-l" + value + " ";
-				else if (it->first == "include") compileFlags += "-I\"" + relPath.string() + "\" ";
-				else if (it->first == "libDir") compileFlags += "-L\"" + relPath.string() + "\" ";
-				else if (it->first == "exclude") excludePaths.push_back(relPath);
-				else if (it->first == "root") includePaths.push_back(relPath);
-				else if (it->first == "out") {
-					compileFlags += "-o\"" + relPath.string() + "\" ";
-					hasOutDir = true;
-				}
-				else if (it->first == "g++flags") compileFlags += value + " ";
-			}
+		const char* profile = "";
+		if (argc > 2) profile = argv[2];
+		
+		std::string buildPath = (arg1.size() > 2) ? arg1.substr(2) + ".ppp" : "build.ppp";
+		svmap objects = parse_file(buildPath.c_str(), profile);
+
+
+		fs::path baseDir;
+		if (objects.count("cwd") == 1) baseDir = fs::absolute(fs::path(objects["out"][0]));
+		else baseDir = fs::path(buildPath).parent_path();
+
+		#define RUN_EACH_VALUE(key) if (objects.count(key) == 1) for (auto v : objects[key])
+
+		RUN_EACH_VALUE("root") rootPaths.push_back(fs::absolute(baseDir / fs::path(v)));
+		RUN_EACH_VALUE("exclude") excludePaths.push_back(fs::absolute(baseDir / fs::path(v)));
+
+		RUN_EACH_VALUE("include") compileFlags += "-I\"" + (baseDir / fs::path(v)).string() + "\" ";
+		RUN_EACH_VALUE("libDir") compileFlags += "-L\"" + (baseDir / fs::path(v)).string() + "\" ";
+		RUN_EACH_VALUE("lib") compileFlags += "-l" + v + " ";
+
+		RUN_EACH_VALUE("define") compileFlags += "-D" + v + " ";
+
+		RUN_EACH_VALUE("g++flags") compileFlags += v + " ";
+
+		if (objects.count("out") == 1) {
+			compileFlags += "-o \"" + fs::absolute(baseDir / fs::path(objects["out"][0])).string() + "\" ";
+			hasOutDir = true;
 		}
+
 	// If flags were instead written in the command line add g++ flags based on them
 	} else {
 
@@ -57,7 +64,7 @@ int main(int argc, char** argv) {
 			// If the arg is '`e' add the following path to the excluded paths list
 			if (arg.substr(0, 2) == "_e") excludePaths.push_back(fs::path(arg.substr(2)));
 			// If the arg is '`i' add the following path to the included paths list
-			else if (arg.substr(0, 2) == "_i") includePaths.push_back(fs::path(arg.substr(2)));
+			else if (arg.substr(0, 2) == "_r") rootPaths.push_back(fs::path(arg.substr(2)));
 			// If it's not a p++ flag pass it to g++
 			else {
 				// Set a flag if the output
@@ -73,45 +80,40 @@ int main(int argc, char** argv) {
 	}
 
 	// Inform the user if no source directories are provided
-	if (includePaths.size() < 1) {
-		usage();
+	if (rootPaths.size() < 1) {
+		printf("usage: p++ requires one (or more) source directory(s)\n");
 		return 0;
 	}
 
 	// If no out dir was specified, set the out to the source root
-	if (!hasOutDir) compileFlags += "\"-o" + includePaths[0].generic_string() + "/a.exe\" ";
+	if (!hasOutDir) compileFlags += "\"-o" + rootPaths[0].generic_string() + "/a.exe\" ";
 
 	// A string to hold all of the file paths
 	std::string fileString = "";
 	
-	for (int i = 0; i < includePaths.size(); i++) {
+	for (int i = 0; i < rootPaths.size(); i++) {
 		// Check if the root directory exists
-		if (!fs::exists(includePaths[i])) {
-			std::cerr << "error: source directory '" << includePaths[i].generic_string() << "' does not exist!\n";
+		if (!fs::exists(rootPaths[i])) {
+			fprintf(stderr, "error: source directory '%s' does not exist!\n", rootPaths[i].c_str());
 			return 0;
 		}
 
 		// Search the source directory and add all relivant files to the list
-		searchDir(includePaths[i], excludePaths, fileString);
+		searchDir(rootPaths[i], excludePaths, fileString);
 	}
 	
 	// Check if there were any c++ files found
 	if(fileString.size() == 0) {
-		std::cerr << "warning: no c++ files found in any of the source directories. program exiting...\n";
+		printf("warning: no c++ files found in any of the source directories. program exiting...\n");
 		return 0;
 	}
 
 	// Report the command to the user
-	std::cout << "> Executing command: g++ " << fileString << compileFlags << "<" << "\n\n";
+	printf("> Executing command: g++ %s<\n\n", (fileString + compileFlags).c_str());
 	// Execute the command
 	system(("g++ " + fileString + compileFlags).c_str());
 
 	return 0;
-}
-
-// A simple usage statment to throw is the user forgot to include soure directories
-void usage() {
-	std::cout << "usage: p++ requires one (or more) source directory(s) with the `i flag [e.g. p++ `ipath/to/your/source/directory/]\n";
 }
 
 void searchDir(fs::path root, std::vector<fs::path> excludePaths, std::string& fileString) {
